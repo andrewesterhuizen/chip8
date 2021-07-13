5b17392310a7cf12b2ce95de4ad1bcd44f2e6cab
Andrew Esterhuizen <andrewesterhuizen@Andrews-MacBook-Pro.local>
2021-07-13 19:40:46 +0100

    Cleanup main.js file in web sandbox

--- web/main.js
+++ web/main.js
@@ -1,26 +1,9 @@
 import RomLoader from "./RomLoader.js";
 import VM from "../lib/VM.js";
-import Assembler from "../lib/Assembler.js";
 
 const main = async () => {
   const romLoader = new RomLoader();
   const rom = await romLoader.load("/c8games/PONG");
-
-  const source = `
-  LD V3, V0
-  LD F, V3
-
-  DRW V0, V1, 0x5
-
-  DRW V0, V1, 0x5
-
-loop:
-  JP loop
-`;
-
-  const assembler = new Assembler(source);
-  // const rom = assembler.getInstructions();
-
   const vm = new VM();
   vm.loadRom(rom);
   vm.start(1 / 60);
